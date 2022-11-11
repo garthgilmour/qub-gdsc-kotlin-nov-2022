6a5e332863c61f961dd8fceb0e9164c9210d79f1
Garth.Gilmour <garth.gilmour@instil.co>
2022-11-11 16:12:24 +0000

    Garths DSL Iteration 4 - complete DSL

--- Play/GarthsMarvelousDSL/src/main/kotlin/Main.kt
+++ Play/GarthsMarvelousDSL/src/main/kotlin/Main.kt
@@ -1,29 +1,52 @@
 
-abstract class Container<T> {
-    protected val children = mutableListOf<T>()
+interface Node {
+    fun marshall(): String
 }
 
-class Course(val title: String): Container<Module>() {
+abstract class ContainerNode<T: Node>(private val tagName: String): Node {
+    private val children = mutableListOf<T>()
+
+    private fun marshallChildren(): String {
+        return children.joinToString(separator="\n") { it.marshall() }
+    }
+
+    override fun marshall() = """
+        |<$tagName>
+        |${marshallChildren()}
+        |</$tagName>
+    """.trimMargin("|")
+
+    protected fun addAndConfigureChild(
+        child: T,
+        action: T.() -> Unit
+    ) = child.apply(action).also { children.add(it) }
+
+    protected fun addChild(child: T) = children.add(child)
+}
+
+class Course(private val title: String): ContainerNode<Module>("html") {
 
     override fun toString() = "Some damn DSL with title $title"
 
     fun module(title: String = "Default title", action: Module.() -> Unit): Module {
-        return Module(title).apply(action).also { children.add(it) }
+        return addAndConfigureChild(Module(title), action)
     }
 }
-class Module(val title: String): Container<Section>() {
+class Module(val title: String): ContainerNode<Section>("section") {
 
     fun section(index: Int = 0, action: Section.() -> Unit): Section {
-        return Section(index).apply(action).also { children.add(it) }
+        return addAndConfigureChild(Section(index), action)
     }
 }
-class Section(val index: Int): Container<Topic>() {
+class Section(val index: Int): ContainerNode<Topic>("article") {
     operator fun String.unaryPlus() {
-        children.add(Topic(this))
+        addChild(Topic(this))
     }
 }
 
-class Topic(val content: String)
+class Topic(private val content: String): Node {
+    override fun marshall() = "<p>$content</p>"
+}
 
 fun course(title: String = "Default title", action: Course.() -> Unit): Course {
     return Course(title).apply(action)
@@ -66,6 +89,6 @@ fun main(args: Array<String>) {
             }
         }
     }
-    println(dsl1)
-    println(dsl2)
+    println(dsl1.marshall())
+    println(dsl2.marshall())
 }
