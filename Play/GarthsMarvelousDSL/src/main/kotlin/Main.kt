07cd73e6b36f4aa1ed11b7e25d2e36389d8cd092
Garth.Gilmour <garth.gilmour@instil.co>
2022-11-11 14:31:53 +0000

    Garths DSL Iteration 1 - Skeleton DSL

---
+++ Play/GarthsMarvelousDSL/src/main/kotlin/Main.kt
@@ -0,0 +1,38 @@
+class Course(val title: String) {
+    override fun toString() = "Some damn DSL with title $title"
+
+    fun module(title: String = "Default title", action: Module.() -> Unit): Module {
+        return Module(title).apply(action)
+    }
+}
+class Module(val title: String) {
+    fun section(index: Int = 0, action: Section.() -> Unit): Section {
+        return Section(index).apply(action)
+    }
+}
+class Section(val index: Int)
+
+fun course(title: String = "Default title", action: Course.() -> Unit): Course {
+    return Course(title).apply(action)
+}
+fun main(args: Array<String>) {
+    println("My DSL For Training Courses")
+
+    val dsl1 = course {
+        module {
+            section {
+
+            }
+        }
+    }
+
+    val dsl2 = course("Kotlin 101") {
+        module("OO In Kotlin") {
+            section(0) {
+
+            }
+        }
+    }
+    println(dsl1)
+    println(dsl2)
+}
